58a83235eae1deca6f8f27c6f58ede3484cef2b2
cPP <huaycoin@betkub.com>
2022-04-29 14:05:37 +0700

    fix: adding puppeteer args

--- src/core/services/getPuppeteerBrowser.ts
+++ src/core/services/getPuppeteerBrowser.ts
@@ -3,8 +3,8 @@ import puppeteer, { LaunchOptions } from 'puppeteer-core'
 export const getPuppeteerBrowser = async (options?: LaunchOptions) => {
   return await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox'],
-    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : '/usr/bin/google-chrome-stable --no-sandbox',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : '/usr/bin/google-chrome-stable',
     ...options,
   })
 }
