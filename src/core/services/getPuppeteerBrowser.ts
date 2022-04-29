e70981775c188e87b760621c22a34d425bf08938
cPP <huaycoin@betkub.com>
2022-04-29 14:01:41 +0700

    fix: adding sandbox in root state

--- src/core/services/getPuppeteerBrowser.ts
+++ src/core/services/getPuppeteerBrowser.ts
@@ -3,7 +3,8 @@ import puppeteer, { LaunchOptions } from 'puppeteer-core'
 export const getPuppeteerBrowser = async (options?: LaunchOptions) => {
   return await puppeteer.launch({
     headless: true,
-    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : '/usr/bin/google-chrome-stable',
+    args: ['--no-sandbox'],
+    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : '/usr/bin/google-chrome-stable --no-sandbox',
     ...options,
   })
 }
