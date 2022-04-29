9c3970677d085deafb667d38b86419df9b6c1070
cPP <huaycoin@betkub.com>
2022-04-29 13:23:35 +0700

    adding google-chrome-stable path

--- src/core/services/getPuppeteerBrowser.ts
+++ src/core/services/getPuppeteerBrowser.ts
@@ -1,11 +1,9 @@
 import puppeteer, { LaunchOptions } from 'puppeteer-core'
-import chrome from 'chrome-aws-lambda'
 
 export const getPuppeteerBrowser = async (options?: LaunchOptions) => {
   return await puppeteer.launch({
     headless: true,
-    args: chrome.args,
-    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : await chrome.executablePath,
+    executablePath: process.env.NODE_ENV === 'development' ?  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge' : '/usr/bin/google-chrome-stable',
     ...options,
   })
 }
