1539afcd85717cb2ebc6986edd64e45d0cebfe5f
cPP <huaycoin@betkub.com>
2022-05-06 10:41:54 +0700

    feat: caching lists

--- src/pages/api/v1/list/[page].ts
+++ src/pages/api/v1/list/[page].ts
@@ -1,10 +1,17 @@
 import { NextApiHandler } from 'next'
+import fs from 'fs'
+import path from 'path'
 
 import { getList } from '../../../../core/services/getList'
+import dayjs from 'dayjs';
 
 const api: NextApiHandler = async (req, res) => {
+  let cachedData;
+  const [day, month] = dayjs().format('DD-MM').split('-')
+
   try {
     const page = req.query?.page ?? "1"
+    const LIST_CACHE_PATH = Number(day) <= 15 ? `.page-${page}-first-${month}` : `.page-${page}-second-${month}`
 
     if (!Number.isSafeInteger(Number(page))) {
       return res.status(400).send({
@@ -12,16 +19,35 @@ const api: NextApiHandler = async (req, res) => {
         response: 'invalid positive integer'
       })
     } else {
-      const targetPage = Number(page)
+      try {
+        cachedData = JSON.parse(
+          fs.readFileSync(path.join(__dirname, LIST_CACHE_PATH), 'utf8')
+        )
+        console.log("cachedData", LIST_CACHE_PATH, cachedData)
+      } catch (error) {
+        console.log('Lists cache not initialized')
+      }
 
-      const lists = await getList(targetPage)
+      const targetPage = Number(page)
+      let lists;
+      if (!cachedData) {
+        lists = await getList(targetPage)
+
+        fs.writeFileSync(
+          path.join(__dirname, LIST_CACHE_PATH),
+          JSON.stringify(lists),
+          'utf8'
+        )
+        console.log('Wrote to lotteries cache')
+        cachedData = lists
+      }
 
       res.setHeader('Cache-Control', 's-maxage=7200')
       res.setHeader('Access-Control-Allow-Origin', '*')
 
       return res.send({
         status: 'success',
-        response: lists,
+        response: cachedData,
       })
     }
   } catch (e) {
